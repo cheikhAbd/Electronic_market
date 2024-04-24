cec2331ce7c5a4cc6747828fb7161ee1f6808626
cheikhAbd <chabd122@gmail.com>
2024-04-24 15:10:06 +0000

    La premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './component/content/content.component';
+import { SideBarComponent } from './component/side-bar/side-bar.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    SideBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
